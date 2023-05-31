ab8231f0deead55ce9af9f0bc34c959029811407
Raz Nitzan <razn@cyber.org.il>
2023-05-31 10:11:36 +0300

    revert version number

--- inginious/__init__.py
+++ inginious/__init__.py
@@ -14,8 +14,7 @@ except ImportError:
     from importlib_metadata import version, PackageNotFoundError
 
 try:
-    # __version__ = version(__name__)
-    __version__ = "0.8.dev0"
+    __version__ = version(__name__)
 except PackageNotFoundError:
     __version__ = "0.7.dev0"
 
