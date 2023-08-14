12f3b7fd98794ac62843b03794888a98b163a36e
Raz Nitzan <razn@cyber.org.il>
2023-08-14 08:15:35 +0300

    cature os release

--- inginious/frontend/app.py
+++ inginious/frontend/app.py
@@ -183,7 +183,7 @@ def get_app(config):
     builtins.__dict__['_'] = l10n_manager.gettext
 
     try:
-        os_version = os.system(r'cat /etc/os-release | grep PRETTY | cut -d= -f2')
+        os_version = subprocess.run(['cat', r'/etc/os-release | grep PRETTY | cut -d= -f2'], capture_output=True))
     except:
         os_version = "other OS"
     
