0dc0c8aa4a75f731614f0198beab15f44c73f29f
Raz Nitzan <razn@cyber.org.il>
2023-08-19 06:23:52 +0300

    remove html template from inputs

--- inginious/frontend/pages/tasks.py
+++ inginious/frontend/pages/tasks.py
@@ -221,9 +221,6 @@ class BaseTaskPage(object):
                     debug = "ssh"
                 del userinput['@debug-mode']
 
-            if len(task._problems) > 0 and task._problems[0].get_type() == 'code':
-                    userinput = self.add_feedback_html_to_user_input(userinput, taskid, task_type)
-
             # Start the submission
             try:
                 submissionid, oldsubids = self.submission_manager.add_job(task, userinput, debug)
@@ -413,40 +410,6 @@ class BaseTaskPage(object):
             return sorted_new_data
         return data
 
-    def add_feedback_html_to_user_input(self, user_input, task_id, task_type):
-        try:
-
-            # couldn't open with  get_renderer, errors on js, tries to render the page and run the js
-            feedback_file_name = self.get_feedback_file_name(task_type)
-            file_path = self.template_helper._root_path + '/'+ self.template_helper._template_dir + '/task_page/' +feedback_file_name
-            with codecs.open(file_path,'r',encoding='utf8') as f:
-                feedback_html = f.read()
-            # todo, in order to support multiple scenario boxes in the same html page,
-            # it might be a good idea to render the html with the task id in it (05-06, for example)
-            # that way, the way the js will render in the appropriate modal
-            feedback_html_injected_with_id = feedback_html.replace('task_id_to_replace', task_id)
-            feedback_html_injected_with_id = '.. raw:: html' + '\n' + self.indent(feedback_html_injected_with_id, 4)
-            user_input['html_template'] = feedback_html_injected_with_id
-        except Exception as err:
-            prefered_encoding = locale.getpreferredencoding()
-            self.logger.error( ' ---- prefered_encoding ' + repr(prefered_encoding))
-            # text = 'error template_helper --- ' + repr(err)
-            # self.logger.error(text)
-            self.logger.error('traceback data is ' + traceback.format_exc())
-            user_input['html_template'] = ''
-
-        return user_input
-
-
-    def get_feedback_file_name(self, task_type):
-        if task_type == 'python-unit-test':
-            return 'feedback_python.html'
-
-        return 'feedback.html'
-    
-    def indent(self, text, amount, ch=' '):
-        padding = amount * ch
-        return ''.join(padding + line for line in text.splitlines(True))
 
 class TaskPageStaticDownload(INGIniousPage):
     """ Allow to download files stored in the task folder """
