a74e7db2db3930e862d5f8e17c462e088fdd0f95
Raz Nitzan <razn@cyber.org.il>
2023-09-11 13:16:39 +0300

    fix lessons list

--- inginious/frontend/plugins/bulk_date_change/__init__.py
+++ inginious/frontend/plugins/bulk_date_change/__init__.py
@@ -11,62 +11,23 @@ MANUAL_DIR_PATH = os.path.dirname(os.path.abspath(__file__))
 
 class BulkDateChangePage(INGIniousAdminPage):
     """ Bulk Dadeline change """
-
-    """ Get all lessons """
-    def get_lessons(self, course):
-        tasks = course.get_tasks()
-        lessons_list = set()
-        lessons = []
-
-        for task in tasks:
-            lessons_list.add(get_task_and_lesson(task)[0])
-
-        if lessons_list:
-            # sorted_lessons = sorted(lessons_list, key=int)
-            sorted_lessons = sorted(lessons_list)
-            lessons = OrderedDict([(lesson, {"name": lesson,
-                                             "tasks": []}) for lesson in sorted_lessons])
-        for task in tasks:
-            lesson_num, task_num = get_task_and_lesson(task)
-
-            lessons[lesson_num]['tasks'].append({"id": task_num,
-                                                 "taskid": tasks[task]})
-
-        return lessons
-
-    def get_tasks(self, course):
-        # Get Tasks
-        files = self.task_factory.get_readable_tasks(course)
-        output = {}
-        errors = []
-        for task in files:
-            try:
-                output[task] = course.get_task(task)
-            except Exception as inst:
-                errors.append({"taskid": task, "error": str(inst)})
-        return OrderedDict(sorted(list(output.items()), key=lambda t: (t[1].get_order(), t[1].get_id())))
-
-    def get_lesson_tasks(self, course, lesson):
-        tasks = self.get_tasks(course)
-        output = {}
-        pattern = re.compile("^" + lesson)
-
-        for task in tasks:
-            if pattern.match(task):
-                output[task] = course.get_task(task)
-        return output
-
-
     def GET_AUTH(self, courseid):
         course = self.get_course_and_check_rights(courseid, allow_all_staff=True)[0]
-        lessons = self.get_lessons(course)
+        lessons = self._get_lessons(course)
         current_lesson = list(lessons)[0] if len(list(lessons)) > 0 else None
-        tasks = self.get_tasks(course)
+        tasks = self._get_tasks(course)
         min_date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         max_date = (date.today() + timedelta(days=7)).strftime('%Y-%m-%d %H:%M:%S')
 
-        return self.template_helper.get_custom_renderer('frontend/plugins/bulk_date_change')\
-            .admin(course, lessons, current_lesson, self.webterm_link, AccessibleTime, tasks, min_date, max_date)
+        # return self.template_helper.get_custom_renderer('frontend/plugins/bulk_date_change')\
+        #     .admin(course, lessons, current_lesson, self.webterm_link, AccessibleTime, tasks, min_date, max_date)
+        return self.template_helper.render("admin.html", 
+                                    template_folder='frontend/plugins/bulk_date_change',
+                                    course=course, 
+                                    lessons=lessons,
+                                    min_date=min_date,
+                                    max_date=max_date,
+                                    current_lesson=current_lesson)
 
     def POST_AUTH(self, courseid):
         course = self.get_course_and_check_rights(courseid, allow_all_staff=True)[0]
@@ -93,14 +54,58 @@ class BulkDateChangePage(INGIniousAdminPage):
 
         return json.dumps({'status': 'success'})
 
+    """ Get all lessons """
+    def _get_lessons(self, course):
+        tasks = course.get_tasks()
+        lessons_list = set()
+        lessons = []
 
-# according to a naming convention lesson-task
-def get_task_and_lesson(task_name):
-    task_splitted = task_name.split('-')
-    lesson_name = task_splitted[0]
-    task_name = task_splitted[len(task_splitted) - 1]
+        for task in tasks:
+            lessons_list.add(self._get_task_and_lesson(task)[0])
 
-    return lesson_name, task_name
+        if lessons_list:
+            # sorted_lessons = sorted(lessons_list, key=int)
+            sorted_lessons = sorted(lessons_list)
+            lessons = OrderedDict([(lesson, {"name": lesson,
+                                             "tasks": []}) for lesson in sorted_lessons])
+        for task in tasks:
+            lesson_num, task_num = self._get_task_and_lesson(task)
+
+            lessons[lesson_num]['tasks'].append({"id": task_num,
+                                                 "taskid": tasks[task]})
+
+        return lessons
+
+    def _get_tasks(self, course):
+        # Get Tasks
+        files = self.task_factory.get_readable_tasks(course)
+        output = {}
+        errors = []
+        for task in files:
+            try:
+                output[task] = course.get_task(task)
+            except Exception as inst:
+                errors.append({"taskid": task, "error": str(inst)})
+        # return OrderedDict(sorted(list(output.items()), key=lambda t: (t[1].get_order(), t[1].get_id())))
+        return OrderedDict(sorted(list(output.items())))
+
+    def _get_lesson_tasks(self, course, lesson):
+        tasks = self.get_tasks(course)
+        output = {}
+        pattern = re.compile("^" + lesson)
+
+        for task in tasks:
+            if pattern.match(task):
+                output[task] = course.get_task(task)
+        return output
+
+    # according to a naming convention lesson-task
+    def _get_task_and_lesson(self, task_name):
+        task_splitted = task_name.split('-')
+        lesson_name = task_splitted[0]
+        task_name = task_splitted[len(task_splitted) - 1]
+
+        return lesson_name, task_name
 
 
 def add_admin_menu(course):
@@ -123,4 +128,4 @@ def init(plugin_manager, _, _2, _3):
     plugin_manager.add_hook('course_admin_main_menu', add_admin_menu)
     plugin_manager.add_hook('javascript_header', add_js_file)
     plugin_manager.add_hook('css', add_css_file)
-    plugin_manager.add_page("/admin/<coursid>/bulk_date_change", BulkDateChangePage.as_view('bulk_date_change'))
+    plugin_manager.add_page("/admin/<courseid>/bulk_date_change", BulkDateChangePage.as_view('bulk_date_change'))
