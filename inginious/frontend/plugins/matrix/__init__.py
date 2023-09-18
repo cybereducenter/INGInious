03ab63a50ae07bba2bcfc163fd26647d5d31aec5
darias <darias@comm-it.com>
2023-09-18 13:16:19 +0300

    add file to feedback submission

--- inginious/frontend/plugins/matrix/__init__.py
+++ inginious/frontend/plugins/matrix/__init__.py
@@ -291,7 +291,7 @@ class MergeFeedbackPage(INGIniousAdminPage):
                                     100 * feedback_categories[category]['status']['passed'] / feedback_categories[category]['status']['total']
                                 ) if feedback_categories[category]['status']['total'] else 0
 
-            user_input = task.adapt_input_for_backend(user_input)
+            # user_input = task.adapt_input_for_backend(user_input)
             # if not task.input_is_consistent(user_input, self.default_allowed_file_extensions, self.default_max_file_size):
             #     raise APIInvalidArguments()
             self.user_manager.user_saw_task(username, courseid, task.get_id())
